5884e0f1b9e5f78002916cf2b562fe882b6d2920
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-12 20:47:01 -0500

    add text input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-extern crate azul;
-
-use azul::{prelude::*, widgets::{label::Label, button::Button}};
+use azul::{
+    prelude::*,
+    widgets::{
+        label::Label,
+        button::Button,
+        text_input::TextInput,
+        text_input::TextInputState,
+    },
+};
 
 struct MyDataModel {
     counter: usize,
+    txt_state: TextInputState,
 }
 
 fn update_counter(app_state: &mut AppState<MyDataModel>, _event: WindowEvent<MyDataModel>) -> UpdateScreen {
@@ -12,21 +19,30 @@ fn update_counter(app_state: &mut AppState<MyDataModel>, _event: WindowEvent<MyD
 }
 
 impl Layout for MyDataModel {
-    fn layout(&self, _: WindowInfo<Self>) -> Dom<Self> {
+    fn layout(&self, info: WindowInfo<Self>) -> Dom<Self> {
         let lbl = Label::new(format!("{}", self.counter))
             .dom();
         let btn = Button::with_label("Update counter")
             .dom()
             .with_callback(On::MouseUp, Callback(update_counter));
 
+        let txt: Dom<MyDataModel> = TextInput::new()
+            .bind(info.window, &self.txt_state, &self)
+            .dom(&self.txt_state);
+
         Dom::new(NodeType::Div)
             .with_child(lbl)
             .with_child(btn)
+            .with_child(txt)
     }
 }
 
 fn main() {
-    let app = App::new(MyDataModel { counter: 0 }, AppConfig::default());
+    let dm = MyDataModel {
+        counter: 0,
+        txt_state: TextInputState::new("Hello?")
+    };
+    let app = App::new(dm, AppConfig::default());
     let window = Window::new(WindowCreateOptions::default(), Css::native()).unwrap();
     app.run(window).unwrap();
 }
